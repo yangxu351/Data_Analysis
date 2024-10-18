70aefee69ee573978f136ee3f379066ca5acbc13
yangxu351 <1554404832@qq.com>
2024-10-18 14:06:20 +0800

    change dir

--- analysis_scripts/integrity/odt_integrity.py
+++ analysis_scripts/integrity/odt_integrity.py
@@ -15,12 +15,12 @@ def does_match_img_lbl(base_dir, split):
     if len(lbl_list)>len(img_list):
         lbl_woimg_list = [os.path.join(lbl_dir, name) for name in lbl_list if name.split('.')[0] not in img_name_wo_suffix]
         arr_lbl_woimg = np.array(lbl_woimg_list)
-        np.savetxt(os.path.join(base_dir, split, 'lbl_without_img.txt'), arr_lbl_woimg, delimiter='\n')
+        np.savetxt(os.path.join(base_dir, f'{split}_lbl_without_img.txt'), arr_lbl_woimg, delimiter='\n')
 
     elif len(lbl_list)<len(img_list):
         img_wolbl_list = [os.path.join(img_dir, name) for name in img_list if name.split('.')[0] not in lbl_name_wo_suffix]
         arr_img_wolbl = np.array(img_wolbl_list)
-        np.savetxt(os.path.join(base_dir, split, 'img_without_lbl.txt'), arr_img_wolbl, delimiter='\n')
+        np.savetxt(os.path.join(base_dir, f'{split}_img_without_lbl.txt'), arr_img_wolbl, delimiter='\n')
 
 if __name__ == '__main__':
     base_dir = 'F:/Public_Dataset/COCO'
