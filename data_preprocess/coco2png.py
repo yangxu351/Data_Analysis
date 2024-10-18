c1baff6ed4f4d432f595a101b0c1e7831549e91f
yangxu351 <1554404832@qq.com>
2024-10-18 13:57:06 +0800

    duplicate check

--- data_preprocess/coco2png.py
+++ data_preprocess/coco2png.py
@@ -30,16 +30,70 @@ def rle_decode(counts, mask, cid):
 
 
 def convert_coco_to_png(base_dir, src_split='val2017', split='val'):
+    '''
+        covert coco to png
+        find duplicate img
+        find duplicate cat
+    '''
     js_file = os.path.join(base_dir, 'annotations', f'instances_{src_split}.json')
     js = json.load(open(js_file))
     img_list = js['images']
     dict_img_wh = {}
     dict_img_id_name = {}
+    duplicate_imginfo_list = []
     for img_info in img_list:
+        # FIXME: check duplicate?
+        img_id = img_info['id']
+        if img_id in dict_img_id_name.keys():
+            print(f'label info of image {img_id} is duplicate!')
+            duplicate_imginfo_list.append(img_id)
+            continue # duplicate is ignored
+
         dict_img_wh[img_info['id']] = (img_info['width'], img_info['height'])
         dict_img_id_name[img_info['id']] = img_info['file_name']
+    
+    dumplicat_imginfo_file = os.path.join(base_dir, f'{split}_duplicate_imginfo.txt')
+    with open(dumplicat_imginfo_file, 'w') as f:
+        for imgid in duplicate_imginfo_list:
+            f.write("%d\n" % (imgid))
+    f.close()
+
     with open(os.path.join(base_dir, 'dict_img_id_name.json'), 'w') as f: # dict of img_id:img_name
         json.dump(dict_img_id_name, f, ensure_ascii=False, indent=3)
+    f.close() 
+
+    categories = js['categories']
+    dict_cats_super_children = {}
+    dict_cat_id_name = {}
+    duplicate_catinfo_list = []
+    for cat_info in categories:
+        cat_id = cat_info['id']
+        cat_name = cat_info['name']
+        # FIXME: check duplicate?
+        if cat_id in dict_cat_id_name.keys():
+            print(f'label info of category {cat_id} is duplicate!')
+            duplicate_catinfo_list.append(cat_id)
+            continue # duplicate is ignored
+
+        dict_cat_id_name[cat_id] = cat_name
+        super_cat = cat_info['supercategory']
+        if super_cat not in dict_cats_super_children.keys():
+            dict_cats_super_children[super_cat] = [cat_name]
+        else:
+            dict_cats_super_children[super_cat].append(cat_name)
+
+    dumplicat_catinfo_file = os.path.join(base_dir, f'{split}_duplicate_catinfo.txt')
+    with open(dumplicat_catinfo_file, 'w') as f:
+        for catid in duplicate_catinfo_list:
+            f.write("%d\n" % (catid))
+    f.close()
+
+    with open(os.path.join(base_dir, f'{split}_dict_cat_super_children.json'), 'w') as f: # dict of supercategory:[cat1,cat2,...]
+        json.dump(dict_cats_super_children, f, ensure_ascii=False, indent=3)
+    f.close() 
+
+    with open(os.path.join(base_dir, f'{split}_dict_cat_id_name.json'), 'w') as f: # dict of cat_id:cat_name
+        json.dump(dict_cat_id_name, f, ensure_ascii=False, indent=3)
     f.close() 
 
     anns_list = js['annotations']
