43b10a94ec80d68ede9165a5fa8450bcc1fe985d
yangxu351 <1554404832@qq.com>
2024-10-23 09:26:02 +0800

    seg id check

--- analysis_scripts/correctness/seg_correct.py
+++ analysis_scripts/correctness/seg_correct.py
@@ -23,13 +23,13 @@ def outlier_unreadable_img_detection(base_dir, outlier_thresh, abnormal_conditio
                 except:
                     print('not image')
                     img = None
-                    not_img_list.append(img_file)
+                    not_img_list.append(i_name)
                     continue
                 # print(i_name, 'shape', img.shape) # h, w, c
                 total_num = img.shape[0]*img.shape[1]*img.shape[2]
                 abnormal_ratio = np.count_nonzero(img==abnormal_condition)/(total_num*1.)
                 if abnormal_ratio >= outlier_thresh:
-                    outlier_list.append(img_file) 
+                    outlier_list.append(i_name) 
                     
             with open(os.path.join(base_dir, f'{split}_not_readable_imgs.txt'), 'w') as f:
                 for name in not_img_list:
@@ -65,11 +65,11 @@ def empty_unreadable_check_msk_png(base_dir):
                 except:
                     print('not image')
                     img = None
-                    unreadble_files.append(msk_file)
+                    unreadble_files.append(msk_name)
                     continue
                 empty = np.all(img==0) 
                 if empty:
-                    empty_files.append(msk_file)
+                    empty_files.append(msk_name)
 
             with open(os.path.join(base_dir, f'{split}_empty_masks.txt'), 'w') as f:
                 for name in empty_files:
@@ -80,6 +80,28 @@ def empty_unreadable_check_msk_png(base_dir):
                 for name in unreadble_files:
                     f.write("%s\n" % name)
             f.close()
+
+
+def check_msk_id(base_dir, num_cls):
+    split_folders = os.listdir(base_dir)
+    for sf in split_folders:
+        if not os.path.isdir(os.path.join(base_dir, sf)):
+            continue
+        invalid_msk_id_files = []
+        if split in ['train', 'val', 'test']:
+            msk_dir = os.path.join(base_dir, split, 'masks')
+            msk_list = os.listdir(msk_dir)
+
+            for msk_name in msk_list:
+                msk_file = os.path.join(msk_dir, msk_name)
+                img = np.array(Image.open(msk_file))
+                if img.min()<0 or img.max()>=num_cls:
+                    invalid_msk_id_files.append(msk_name)
+
+        with open(os.path.join(base_dir, f'{split}_invalid_msk_id_files.txt'), 'w') as f:
+            for name in invalid_msk_id_files:
+                f.write("%s\n" % (name))
+        f.close()
 
 if __name__ == "__main__":
     base_dir = 'F:/Public_Dataset/Lunar'
