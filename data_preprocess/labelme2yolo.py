9afa4e5b040ad6646e6409343f4c1eacc7e69700
yangxu351 <1554404832@qq.com>
2025-01-24 11:07:43 +0800

    add 标注文件损坏率 标注类别错误率 标注重复率 空标注 宽高比失调率

--- data_preprocess/labelme2yolo.py
+++ data_preprocess/labelme2yolo.py
@@ -16,43 +16,33 @@ def make_folder_if_not(dst_dir, rm_exist=True):
             os.mkdir(dst_dir)
 
 
-def convert_labelme_to_yolo(labelme_json_path, output_dir):
-    # 类别名称到类别 ID 的映射，需要根据实际情况修改
-    class_mapping = {"person": 0, "car": 1, "dog": 2}  # 示例类别映射，可按需添加更多类别
+def dose_match_img_lbl(base_dir, dict_stat):
+    json_dir = os.path.join(base_dir, 'annotations')
+    img_dir = os.path.join(base_dir, 'images')
+    
+    img_list = os.listdir(img_dir)
+    img_list.sort()
+    img_name_wo_suffix = [f.split('.')[0] for f in img_list]
+    img_wolbl_list = None 
 
-    with open(labelme_json_path, 'r') as f:
-        labelme_data = json.load(f)
+    lbl_list = os.listdir(json_dir)
+    lbl_list.sort()
+    lbl_name_wo_suffix = [f.split('.')[0] for f in lbl_list]
+    lbl_woimg_list = None 
 
-    image_width = labelme_data["imageWidth"]
-    image_height = labelme_data["imageHeight"]
-    shapes = labelme_data["shapes"]
-
-    yolo_annotations = []
-    for shape in shapes:
-        label = shape["label"]
-        class_id = class_mapping.get(label, -1)  # 获取类别 ID，如果类别不在映射中则为 -1
-        if class_id == -1:
-            continue  # 跳过未在映射中的类别
-
-        points = shape["points"]
-        x_min = min([point[0] for point in points])
-        y_min = min([point[1] for point in points])
-        x_max = max([point[0] for point in points])
-        y_max = max([point[1] for point in points])
-
-        # 坐标转换为 YOLO 格式
-        x_center = (x_min + x_max) / 2 / image_width
-        y_center = (y_min + y_max) / 2 / image_height
-        width = (x_max - x_min) / image_width
-        height = (y_max - y_min) / image_height
-
-        yolo_annotations.append(f"{class_id} {x_center} {y_center} {width} {height}")
-
-    image_filename = os.path.basename(labelme_data["imagePath"]).split('.')[0]
-    yolo_txt_path = os.path.join(output_dir, image_filename + ".txt")
-    with open(yolo_txt_path, 'w') as f:
-        for annotation in yolo_annotations:
-            f.write(annotation + '\n')
+    file_names = [x for x in img_name_wo_suffix if x in lbl_name_wo_suffix]
+    if len(lbl_list)>len(file_names):
+        logger.warning('labels more than images!!!')
+        lbl_woimg_list = [name for name in lbl_list if name.split('.')[0] not in img_name_wo_suffix]
+        arr_lbl_woimg = np.array(lbl_woimg_list)
+        np.savetxt(os.path.join(base_dir, 'dismatch_lbl_without_img.csv'), arr_lbl_woimg, fmt='%s')
+        dict_stat['图像标注不匹配率'] = f'{round(len(lbl_woimg_list)/len(lbl_list),4)*100}%'
+    elif len(img_list)>len(file_names):
+        logger.warning('images more than labels!!!')
+        img_wolbl_list = [name for name in img_list if name.split('.')[0] not in lbl_name_wo_suffix]
+        arr_img_wolbl = np.array(img_wolbl_list)
+        np.savetxt(os.path.join(base_dir, 'dismatch_img_without_lbl.csv'), arr_img_wolbl, fmt='%s')
+        dict_stat['图像标注不匹配率'] = f'{round(len(img_wolbl_list)/len(img_list),4)*100}%'
 
 
 
@@ -69,24 +59,33 @@ def covert_labelme_to_yolo(base_dir, dict_stat, wh_thres=50, mapping_file='', ex
     json_files = os.listdir(json_dir)
     dict_id_cat = json.load(open(mapping_file))
     classes = [x for x in dict_id_cat.values()]
-    ids = [x for x in dict_id_cat.keys()]
+    ids = [int(x) for x in dict_id_cat.keys()]
     dict_cat_id = {classes[ix]:ids[ix] for ix in range(len(classes))}
 
     index = 0
     dict_duplicate_file_box = {}
     empty_lbls = []
     dict_invalid_bbx_jsons = {}
+    error_wh_ratio_jsons = []
     suffix_error_files = []
     attribute_lost_jsons = []
     invalid_cat_files = []
+    damaged_files = []
     
     for file_name in json_files:
         if not file_name.endswith(extention):
             suffix_error_files.append(file_name)
+            logger.error(f'{file_name} extention error!')
             continue
         xf = os.path.join(json_dir, file_name)
-        with open(xf, 'r') as f:
-            labelme_data = json.load(f)
+        try:
+            with open(xf, 'r') as f:
+                labelme_data = json.load(f)
+        except:
+            logger.error(f'labelme file {file_name} is not readable!')
+            damaged_files.append(file_name)
+            continue
+
         try:
             image_width = labelme_data["imageWidth"]
             image_height = labelme_data["imageHeight"]
@@ -127,11 +126,16 @@ def covert_labelme_to_yolo(base_dir, dict_stat, wh_thres=50, mapping_file='', ex
             width = x_max - x_min
             height = y_max - y_min
             # check invlid coordinates
-            if x_min < 0 or y_min < 0 or x_max >image_width or y_max >image_height or width<=0 or height<=0 or (max(width/height, height/width) > wh_thres):
+            if x_min < 0 or y_min < 0 or x_max >image_width or y_max >image_height or width<=0 or height<=0:
                 if file_name not in dict_invalid_bbx_jsons.keys():
                     dict_invalid_bbx_jsons[file_name]= [[class_id, x_min, y_min, x_max, y_max]]
                 else:
                     dict_invalid_bbx_jsons[file_name].append([class_id, x_min, y_min, x_max, y_max])
+                logger.error(f'{file_name} contains invalid coordinates!')
+                continue
+            elif max(width/height, height/width) > wh_thres:
+                error_wh_ratio_jsons.append(file_name)
+                logger.error(f'{file_name} contains invalid wh ratio!')
                 continue
 
             # 坐标转换为 YOLO 格式
@@ -163,6 +167,20 @@ def covert_labelme_to_yolo(base_dir, dict_stat, wh_thres=50, mapping_file='', ex
     f.close()
     dict_stat['标注文件格式不一致比例'] = f'{round(len(suffix_error_files)/len(json_files),4)*100}%'   # 标注文件格式不一致比例
 
+    file_damaged_json = os.path.join(base_dir, 'all_jsons_damaged.json')
+    with open(file_damaged_json, 'w') as f:
+        for name in damaged_files:
+            f.write("%s\n" % name)
+    f.close() 
+    dict_stat['标注文件损坏率'] = f'{round(len(damaged_files)/len(json_files),4)*100}%' # 标注文件损坏率
+
+    file_err_cat_json = os.path.join(base_dir, 'all_jsons_err_catid.json')
+    with open(file_err_cat_json, 'w') as f:
+        for name in invalid_cat_files:
+            f.write("%s\n" % name)
+    f.close() 
+    dict_stat['标注类别错误率'] = f'{round(len(invalid_cat_files)/len(json_files),4)*100}%' # 标注类别错误率
+
     attribute_lost_file = os.path.join(base_dir, 'all_jsons_lost_key_attributes.txt')
     with open(attribute_lost_file, 'w') as f:
         for name in attribute_lost_jsons:
@@ -175,38 +193,27 @@ def covert_labelme_to_yolo(base_dir, dict_stat, wh_thres=50, mapping_file='', ex
         json.dump(dict_invalid_bbx_jsons, f, ensure_ascii=False, indent=3)
     f.close()   
     dict_stat['标注不合理比例'] = f'{round(len(dict_invalid_bbx_jsons.keys())/len(json_files),4)*100}%' # 标注不合理比例
+    
+    file_err_wh = os.path.join(base_dir, 'all_jsons_error_wh_ratio.txt') 
+    with open(file_err_wh, 'w') as f:
+        for name in error_wh_ratio_jsons:
+            f.write("%s\n" % name)
+    f.close()   
+    dict_stat['目标边界框宽高比失调率'] = f'{round(len(empty_lbls)/len(json_files),4)*100}%'  # 标注宽高比不合理比例
 
     file_empty = os.path.join(base_dir, 'all_jsons_without_lbls.txt')
     with open(file_empty, 'w') as f:
         for name in empty_lbls:
             f.write("%s\n" % name)
-    f.close()   
+    f.close()           
     dict_stat['空标注比例'] = f'{round(len(empty_lbls)/len(json_files),4)*100}%'  # 空标注比例
 
     with open(os.path.join(base_dir, 'all_jsons_duplicate_bbox.json'), 'w') as f: # dict of file_name:[bbox]
         json.dump(dict_duplicate_file_box, f, ensure_ascii=False, indent=3)
     f.close() 
-    dict_stat['标注重复比例'] = f'{round(len(dict_duplicate_file_box.keys())/len(json_files),4)*100}%'  # 空标注比例
+    dict_stat['标注重复率'] = f'{round(len(dict_duplicate_file_box.keys())/len(json_files),4)*100}%'  # 标注重复率
 
-def movefiles_by_setfile(base_dir, split='train'):
-    '''
-    create new folder and move files to specified folder according to the set file
-    '''
-    set_dir = os.path.join(base_dir, 'ImageSets/Main')
-    img_dir = os.path.join(base_dir, 'JPEGImages')
-    txt_dir = os.path.join(base_dir, 'labelsAll') 
 
-    set_file = os.path.join(set_dir, f'{split}.txt')
-    df_set = pd.read_csv(set_file, header=None)
-    img_foler = os.path.join(base_dir, split, 'images')
-    make_folder_if_not(img_foler)
-    lbl_foler = os.path.join(base_dir, split, 'labels')
-    make_folder_if_not(lbl_foler)
-    for name in df_set.iloc[:,0]:
-        img_name = f'{name}.jpg'
-        lbl_name = f'{name}.txt'
-        shutil.copy(os.path.join(img_dir, img_name), os.path.join(img_foler, img_name))
-        shutil.copy(os.path.join(txt_dir, lbl_name), os.path.join(lbl_foler, lbl_name))
 
 if __name__ == "__main__":
     base_dir = 'F:/Public_Dataset/BCCD_VOC'
@@ -214,10 +221,6 @@ if __name__ == "__main__":
     
     covert_labelme_to_yolo(base_dir)
 
-    
-    movefiles_by_setfile(base_dir, split='train')
-    movefiles_by_setfile(base_dir, split='val')
-    movefiles_by_setfile(base_dir, split='test')
     
     
         
